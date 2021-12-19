c964b0d093d2445bc8dd248575770b79a4f2b28b
kitaharas <mizupe1223@gmail.com>
2021-12-20 00:00:36 +0900

    form_for_create_render

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,23 @@
 class HomeController < ApplicationController
   def index
+    p "-------------------"
+    p params
+    p "-------------------"
+    @user = User.new
   end
-
+  
+  
+  
   def about
   end
-
+  
   def feel
   end
-
+  
   def genre
   end
+  
 
-  def mypage
-  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  
   root 'home#index'
   get 'about', to: 'home#about'
   get 'feel', to: 'home#feel'
   get 'genre', to: 'home#genre'
-  get 'mypage', to: 'home#mypage'
+  get 'users/:id', to: 'users#show', as: 'mypage'
+
+  resources :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
