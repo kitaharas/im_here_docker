bfb308dbd04d06e8d9b9d301937bed53e9f80a7f
kitaharas <mizupe1223@gmail.com>
2022-01-09 17:25:23 +0900

    follower_viewer

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -12,15 +12,15 @@ class RelationshipsController < ApplicationController
     redirect_to request.referer
   end
 
-  # フォロー一覧
-  def followings
-    user = User.find(params[:user_id])
-    @users = user.followings
-  end
+  # # フォロー一覧
+  # def followings
+  #   user = User.find(params[:user_id])
+  #   @users = user.followings
+  # end
 
   # フォロワー一覧
-  def followers
-    user = User.find(params[:user_id])
-    @users = user.followers
-  end
+  # def followers
+  #   user = User.find(params[:user_id])
+  #   @users = user.followers
+  # end
 end
--- app/helpers/relationships_helper.rb
+++ app/helpers/relationships_helper.rb
@@ -1,8 +1,18 @@
 module RelationshipsHelper
 
-  def follower
+  def following
     @user = User.find(params[:id])
     followers = @user.followings.where.not(id: current_user.id)
+    # p "------------"
+    # p followers
+    # p "------------"
   end
+
+  def followers
+    @user = User.find(params[:id])
+    followers = @user.followers.where.not(id: current_user.id)
+  end
+
+
 
 end
